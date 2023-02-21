d2774c3c50354bea2b33c30a34613d539b035585
AidanMountain <34258373+AidanMountain@users.noreply.github.com>
2023-02-21 16:22:32 -0500

    added arrayList for users input

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,10 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        //VendingMachine vendingMachine;
+
         //System.out.println("Hello world!");
+
+
 
     }
 }
--- src/VendingMachine.java
+++ src/VendingMachine.java
@@ -1,20 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 public class VendingMachine {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        List order = new ArrayList();
         Scanner scan = new Scanner(System.in);
-        
+
         System.out.println("Welcome to Aiden and Caden's Vending Machine!");
         //Lists items (you can add more if you want)
         System.out.println("List of items: \n 1. Lays Chips - $2.00 \n 2. Cheetos - $2.00 \n 3. Cliff Bars - $1.00 \n 4. PopTarts - $2:50 \n 5. Planters Trail Mix - $3.50");
         System.out.println("What item do you want to add?");
         int snackChoice = scan.nextInt();
         Snack newSnack = new Snack(snackChoice);
-        System.out.println("Do you want to add another item?");
+        order.add(snackChoice);
 
-        System.out.println("Please Insert Exact Dollar Amount:");
+        System.out.println("Do you want to add another item? (Type 'y' for yes or 'n' for no");
         Scanner scanner = new Scanner(System.in);
+        String yesno = scanner.nextLine();
+        if (yesno == "y") {
+
+        }
+
+
+        System.out.println("Please Insert Exact Dollar Amount (Only $1 bills are accepted)");
         float dollarAmount = scanner.nextFloat();
 
-        System.out.println("Balance: "+ dollarAmount);
+        System.out.println("Balance: " + dollarAmount);
+
     }
 }
