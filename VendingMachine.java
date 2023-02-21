fc30e5d8a3b63c3b56ca50af4a1d821d926e4543
AidanMountain <34258373+AidanMountain@users.noreply.github.com>
2023-02-21 14:26:39 -0500

    added scanner for user input

--- VendingMachine.java
+++ VendingMachine.java
@@ -4,8 +4,9 @@ public class VendingMachine {
         //Lists items (you can add more if you want)
         System.out.println("List of items: \n 1. Lays Chips - $2.00 \n 2. Cheetos - $2.00 \n 3. Cliff Bars - $1.00 \n 4. PopTarts - $2:50 \n 5. Planters Trail Mix - $3.50");
 
+        System.out.println("Please Insert Exact Dollar Amount:");
         Scanner scanner = new Scanner(System.in);
-        dollarAmount = scanner.nextLine();
+        float dollarAmount = scanner.nextFloat();
 
         System.out.println("Balance: "+ dollarAmount);
     }
