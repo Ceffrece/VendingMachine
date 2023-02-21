64b13a961b4d796b7605bbea166628ee70bddede
AidanMountain <34258373+AidanMountain@users.noreply.github.com>
2023-02-21 17:46:02 -0500

    resolved errors

--- src/VendingMachine.java
+++ src/VendingMachine.java
@@ -5,7 +5,9 @@ public class VendingMachine {
     public static void main(String[] args) {
         List order = new ArrayList();
         Scanner scan = new Scanner(System.in);
-        double total;
+        double total = 0;
+
+        boolean orderRunning = true;
 
         System.out.println("Welcome to Aiden and Caden's Vending Machine!");
         //Lists Items
@@ -14,40 +16,46 @@ public class VendingMachine {
         int snackChoice = scan.nextInt();
         Snack newSnack = new Snack(snackChoice);
         order.add(snackChoice);
-        total = total + newSnack.getCost();
+        total += newSnack.getCost();
 
-        System.out.println("Do you want to add another item? (Type 'y' for yes or 'n' for no");
+        System.out.println("Do you want to add another item? (Type 'y' for yes or 'n' for no)");
         Scanner scanner = new Scanner(System.in);
         String yesno = scanner.nextLine();
-        while (yesno == "y") {
+        if(yesno == "y") orderRunning = true;
+        else if(yesno == "n") orderRunning = false;
+        while (orderRunning) {
             System.out.println("What item do you want to add?");
             snackChoice = scan.nextInt();
             newSnack = new Snack(snackChoice);
             order.add(snackChoice);
             total = total + newSnack.getCost();
+            System.out.println("Total: " + total);
             System.out.println("Do you want to add another item? (Type 'y' for yes or 'n' for no)");
             scanner = new Scanner(System.in);
             yesno = scanner.nextLine();
+
         }
 
-        System.out.println("\n Your order is: ");
-        for(int count=0; count<order.size(); order++){
-            System.out.println(order.get(count));
-        }
-        System.out.println("Total: " + total);
-        System.out.println("Would you like to abort your purchase? (Type 'y' for yes or 'n' for no)");
-        String quit = scanner.nextLine();
-        if(quit == "y"){
-            System.out.println("Your purchase has been canceled. Have a nice day! \n $" + total + " returned");
-        }
-        else{
-            System.out.println("Please Insert Exact Dollar Amount (Only $1 bills are accepted)");
-            float dollarAmount = scanner.nextFloat();
-    
-            System.out.println("Balance: " + dollarAmount);
-            System.out.println("Your change is " + (total - dollarAmount));
-            System.out.println("Have a nice day!");
-        }
+
+        //if(!orderRunning) {
+            System.out.println("\n Your order is: ");
+            for (int count = 0; count < order.size(); count++) {
+                System.out.println(order.get(count));
+            }
+            System.out.println("Total: " + total);
+            System.out.println("Would you like to abort your purchase? (Type 'y' for yes or 'n' for no)");
+            String quit = scanner.nextLine();
+            if (quit == "y") {
+                System.out.println("Your purchase has been canceled. Have a nice day! \n $" + total + " returned");
+            } else {
+                System.out.println("Please Insert Exact Dollar Amount (Only $1 bills are accepted)");
+                float dollarAmount = scanner.nextFloat();
+
+                System.out.println("Balance: " + dollarAmount);
+                System.out.println("Your change is " + (total - dollarAmount));
+                System.out.println("Have a nice day!");
+            }
+        //}
 
        
 
