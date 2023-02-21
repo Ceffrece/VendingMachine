043ac78bf149c5c41b69dbc22280b70f27874d2a
Ceffrece <cheffrece@quinnipiac.edu>
2023-02-21 18:01:49 -0500

    fixed errors and added a way to check if the customer still owes something

--- src/VendingMachine.java
+++ src/VendingMachine.java
@@ -20,9 +20,8 @@ public class VendingMachine {
 
         System.out.println("Do you want to add another item? (Type 'y' for yes or 'n' for no)");
         Scanner scanner = new Scanner(System.in);
-        String yesno = scanner.nextLine();
-        if(yesno == "y") orderRunning = true;
-        else if(yesno == "n") orderRunning = false;
+        char yesno = scanner.next().charAt(0);
+        
         while (orderRunning) {
             System.out.println("What item do you want to add?");
             snackChoice = scan.nextInt();
@@ -32,8 +31,9 @@ public class VendingMachine {
             System.out.println("Total: " + total);
             System.out.println("Do you want to add another item? (Type 'y' for yes or 'n' for no)");
             scanner = new Scanner(System.in);
-            yesno = scanner.nextLine();
-
+            yesno = scanner.next().charAt(0);
+            if(yesno == 'y') orderRunning = true;
+            else if(yesno == 'n') orderRunning = false;
         }
 
 
@@ -44,15 +44,20 @@ public class VendingMachine {
             }
             System.out.println("Total: " + total);
             System.out.println("Would you like to abort your purchase? (Type 'y' for yes or 'n' for no)");
-            String quit = scanner.nextLine();
-            if (quit == "y") {
+            char quit = scanner.next().charAt(0);
+            if (quit == 'y') {
                 System.out.println("Your purchase has been canceled. Have a nice day! \n $" + total + " returned");
             } else {
                 System.out.println("Please Insert Exact Dollar Amount (Only $1 bills are accepted)");
                 float dollarAmount = scanner.nextFloat();
 
                 System.out.println("Balance: " + dollarAmount);
-                System.out.println("Your change is " + (total - dollarAmount));
+                while(dollarAmount < total){
+                    System.out.println("You still owe $" + (total-dollarAmount)+ ".");
+                    System.out.println("Please Insert Exact Dollar Amount (Only $1 bills are accepted)");
+                    dollarAmount += scanner.nextFloat();
+                }
+                System.out.println("Your change is " + (dollarAmount- total));
                 System.out.println("Have a nice day!");
             }
         //}
