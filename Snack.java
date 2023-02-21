1c9511d3cc21e3a9e310cc71239c5e96aaa6706a
Ceffrece <cheffrece@quinnipiac.edu>
2023-02-21 15:32:23 -0500

    Adds a way for user to input their snack choice, loops it until they want to quit, and adds objects for snacks

---
+++ Snack.java
@@ -0,0 +1,26 @@
+public class Snack {
+    private String name;
+    private double cost;
+    public Snack(int num){
+        if(num == 1){
+            name = "Lays Chips";
+            cost = 2.00;
+        }
+        if(num == 2){
+            name = "Cheetos";
+            cost = 2.00;
+        }
+        if(num == 3){
+            name = "Cliff Bar";
+            cost = 1.00;
+        }
+        if(num == 4){
+            name = "PopTart";
+            cost = 2.50;
+        }
+        if(num == 5){
+            name = "Trail Mix";
+            cost = 3.50;
+        }
+    }
+}
