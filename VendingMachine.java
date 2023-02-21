6a3d094d916eb5e6a0f952cf0f10b9f14a754cef
Ceffrece <cheffrece@quinnipiac.edu>
2023-02-21 09:51:24 -0500

    Adds beginning statements and list of items in vending machine

--- VendingMachine.java
+++ VendingMachine.java
@@ -1,3 +1,7 @@
 public class VendingMachine {
-    
+    public static void main(String[] args){
+        System.out.println("Welcome to Aiden and Caden's Vending Machine!");
+        //Lists items (you can add more if you want)
+        System.out.println("List of items: \n 1. Lays Chips - $2.00 \n 2. Cheetos - $2.00 \n 3. Cliff Bars - $1.00 \n 4. PopTarts - $2:50 \n 5. Planters Trail Mix - $3.50");
+    }
 }
